d4e4fb1d555ca85acfa98b69be2941374a42c841
Brandy Dieterle <brandy.dieterle@gmail.com>
2025-05-30 10:08:56 -0400

    main

--- map.js
+++ map.js
@@ -24,28 +24,78 @@ const eventsArray = [ {
     year: "2025",
     coords: [33.9423, -83.3724],
     city: "Athens, GA",
-    theme: "Agency and Authorship",}];
+    theme: "Agency and Authorship"},
+{
+    uni: "TCU",
+    year: "2024",
+    coords: [32.7079, -97.3628],
+    city: "Forth Worth, TX",
+    theme: "Seriously Digital"},
 
-    
-    //"UC Davis", "East Carolina U", "2020 cancelled", "2021 cancelled", "Michigan State U", "George Mason U", "U of Findlay", "St. John Fisher College", "U of Wisconsin, Stout", "Washington State U"];
+    {
+    uni: "UC Davis",
+    year: "2023",
+    coords: [38.5382, -121.7617],
+    city: "Davis, CA",
+    theme: "To What End?"},
+
+    {
+    uni: "East Carolina U",
+    year: "2022",
+    coords: [35.6075, -77.3672],
+    city: "Greenville, NC",
+    theme: "Practicing Digital Activisms"},
+
+      {
+    uni: "Michigan State U",
+    year: "2019",
+    coords: [42.7251, -84.4791],
+    city: "East Lansing, MI",
+    theme: "Mission Critical"},
+
+          {
+    uni: "George Mason U",
+    year: "2018",
+    coords: [38.8851, -77.1002],
+    city: "Fairfax, VA",
+    theme: "Digital Phronesis"},
+
+    {
+    uni: "U of Findlay",
+    year: "2017",
+    coords: [41.0546, -83.6549],
+    city: "Findlay, OH",
+    theme: "Techne"},
+
+        {
+    uni: "St. John Fisher College",
+    year: "2016",
+    coords: [43.1161, -77.5119],
+    city: "Rochester, NY",
+    theme: "Crossing Wires"},
+{
+    uni: "U of Wisconsin, Stout",
+    year: "2015",
+    coords: [44.5211, -91.5540],
+    city: "Menomonie, WI",
+    theme: "unknown"},
+
+    {
+    uni: "Washing State U",
+    year: "2014",
+    coords: [46.7295, -117.1803],
+    city: "Pullman, WA",
+    theme: "Evolutions, Revolutions, Convolutions"},
+
+];
+
 
 for (let i = 0; i < eventsArray.length; i++) {
 const coords = (eventsArray[i].coords);
 const marker = L.marker(coords).addTo(map).bindPopup(`<b> ${eventsArray[i].year} </b><br>The conference theme was "${eventsArray[i].theme}," and it was held at ${eventsArray[i].uni} in ${eventsArray[i].city}.`); 
-    // Chain .bindPopup().addTo();
+    // Chain .bindPopup().addTo(); above
 }
 
-// Add a marker 
 
-
-//const cw2025Coords = [33.9423, -83.3724];
-//const cw2025Marker = L.marker(cw2025Coords).addTo(map);
-
-// Add a popup to the marker
-//cw2025Marker.bindPopup('<b>2025</b><br>The conference theme was "Agency and Authorship," and it was held at the University of Georgia in Athens, Georgia.');
-
-const cw2024Coords = [32.7079, -97.3628];
-const cw2024Marker = L.marker(cw2024Coords).addTo(map)
-    .bindPopup('<b>2024</b><br>The conference theme was "Agency and Authorship," and it was held at the University of Georgia in Athens, Georgia.'); // Chain .bindPopup().addTo()
 
 }); // End DOMContentLoaded listener
